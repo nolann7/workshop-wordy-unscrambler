52dd9fffac6193dcb3965b70b463dd4d704846ad
nolann7 <nolann709@gmail.com>
2023-06-02 20:23:30 +0300

    wordy init

--- js/app.js
+++ js/app.js
@@ -1,89 +1,86 @@
-import Wordy from "./wordy.js";
+import Wordy from './wordy.js';
 
+document.addEventListener('DOMContentLoaded', async function ready() {
+  var dictionarySelectorEl = document.getElementById('dictionary-selector');
+  var dictionaryEntriesLengthEl = document.getElementById(
+    'dictionary-entries-length',
+  );
+  var loadTimingEl = document.getElementById('load-timing');
+  var enterLettersEl = document.getElementById('enter-letters');
+  var inputLettersEl = document.getElementById('input-letters');
+  var unscrambleTimingEl = document.getElementById('unscramble-timing');
+  var unscrambleBtn = document.getElementById('unscramble-btn');
+  var wordListEl = document.getElementById('word-list');
 
-document.addEventListener("DOMContentLoaded",async function ready(){
-	var dictionarySelectorEl = document.getElementById("dictionary-selector");
-	var dictionaryEntriesLengthEl = document.getElementById("dictionary-entries-length");
-	var loadTimingEl = document.getElementById("load-timing");
-	var enterLettersEl = document.getElementById("enter-letters");
-	var inputLettersEl = document.getElementById("input-letters");
-	var unscrambleTimingEl = document.getElementById("unscramble-timing");
-	var unscrambleBtn = document.getElementById("unscramble-btn");
-	var wordListEl = document.getElementById("word-list");
+  await loadWordList();
 
-	await loadWordList();
+  // once word list is loaded, enable the UI
+  unscrambleBtn.disabled = false;
 
-	// once word list is loaded, enable the UI
-	unscrambleBtn.disabled = false;
+  dictionarySelectorEl.addEventListener('change', loadWordList, false);
+  enterLettersEl.addEventListener('keydown', onKeydown, false);
+  unscrambleBtn.addEventListener('click', unscrambleLetters, false);
 
-	dictionarySelectorEl.addEventListener("change",loadWordList,false);
-	enterLettersEl.addEventListener("keydown",onKeydown,false);
-	unscrambleBtn.addEventListener("click",unscrambleLetters,false);
+  // ********************************
 
+  function onKeydown(evt) {
+    if (evt.key == 'Enter') {
+      unscrambleLetters();
+    }
+  }
 
-	// ********************************
+  function unscrambleLetters() {
+    var letters = enterLettersEl.value.toUpperCase().trim();
+    enterLettersEl.value = letters;
 
-	function onKeydown(evt) {
-		if (evt.key == "Enter") {
-			unscrambleLetters();
-		}
-	}
+    // validate the input
+    if (!/^[A-Z]{3,}$/.test(letters)) {
+      alert('Enter at least 3 letters!');
+      return;
+    }
 
-	function unscrambleLetters() {
-		var letters = enterLettersEl.value.toUpperCase().trim();
-		enterLettersEl.value = letters;
+    var startTiming = performance.now();
+    // find matching words
+    var words = Wordy.findWords(letters);
+    var endTiming = performance.now();
+    printTiming(unscrambleTimingEl, Number(endTiming - startTiming) || 0);
 
-		// validate the input
-		if (!/^[A-Z]{3,}$/.test(letters)) {
-			alert("Enter at least 3 letters!");
-			return;
-		}
+    // any words found?
+    if (words.length > 0) {
+      enterLettersEl.value = '';
+      inputLettersEl.innerHTML = `Input: <strong>${letters}</strong>`;
+      wordListEl.innerHTML = words.join('<br>');
+    } else {
+      inputLettersEl.innerHTML = '';
+      wordListEl.innerHTML = '<strong>--empty--</strong>';
+    }
+  }
 
-		var startTiming = performance.now();
-		// find matching words
-		var words = Wordy.findWords(letters);
-		var endTiming = performance.now();
-		printTiming(unscrambleTimingEl,Number(endTiming - startTiming) || 0);
+  async function loadWordList() {
+    var whichList = dictionarySelectorEl.value;
+    if (['tiny', 'short', 'long'].includes(whichList)) {
+      let wordList = await (await fetch(`${whichList}-wordlist.json`)).json();
 
-		// any words found?
-		if (words.length > 0) {
-			enterLettersEl.value = "";
-			inputLettersEl.innerHTML = `Input: <strong>${letters}</strong>`;
-			wordListEl.innerHTML = words.join("<br>");
-		}
-		else {
-			inputLettersEl.innerHTML = "";
-			wordListEl.innerHTML = "<strong>--empty--</strong>";
-		}
-	}
+      let startTiming = performance.now();
+      let length = Wordy.loadWords(wordList);
+      let endTiming = performance.now();
+      printTiming(loadTimingEl, Number(endTiming - startTiming) || 0);
 
-	async function loadWordList() {
-		var whichList = dictionarySelectorEl.value;
-		if ([ "tiny", "short", "long", ].includes(whichList)) {
-			let wordList = await (await fetch(`${whichList}-wordlist.json`)).json();
+      dictionaryEntriesLengthEl.innerText = `(entries: ${length})`;
 
-			let startTiming = performance.now();
-			let length = Wordy.loadWords(wordList);
-			let endTiming = performance.now();
-			printTiming(loadTimingEl,Number(endTiming - startTiming) || 0);
+      inputLettersEl.innerHTML = '';
+      unscrambleTimingEl.innerHTML = '';
+      wordListEl.innerHTML = '<strong>--empty--</strong>';
+    }
+  }
 
-			dictionaryEntriesLengthEl.innerText = `(entries: ${length})`;
+  function printTiming(timingEl, ms) {
+    ms = Number(ms.toFixed(1));
 
-			inputLettersEl.innerHTML = "";
-			unscrambleTimingEl.innerHTML = "";
-			wordListEl.innerHTML = "<strong>--empty--</strong>";
-		}
-	}
-
-	function printTiming(timingEl,ms) {
-		ms = Number(ms.toFixed(1));
-
-		if (ms >= 50) {
-			timingEl.innerHTML = `(<strong>${(ms / 1000).toFixed(2)}</strong> sec)`;
-		}
-		else {
-			timingEl.innerHTML = `(<strong>${ms.toFixed(1)}</strong> ms)`
-		}
-	}
-
+    if (ms >= 50) {
+      timingEl.innerHTML = `(<strong>${(ms / 1000).toFixed(2)}</strong> sec)`;
+    } else {
+      timingEl.innerHTML = `(<strong>${ms.toFixed(1)}</strong> ms)`;
+    }
+  }
 });
--- js/wordy.js
+++ js/wordy.js
@@ -1,22 +1,21 @@
 export default {
-	loadWords,
-	findWords
+  loadWords,
+  findWords,
 };
-
 
 // ****************************
 
 function loadWords(wordList) {
-	// TODO: implement a data structure for the array
-	// `wordList` parameter; return the number
-	// of entries inserted into the data structure
-	return 0;
+  // TODO: implement a data structure for the array
+  // `wordList` parameter; return the number
+  // of entries inserted into the data structure
+  return 0;
 }
 
 function findWords(input) {
-	// TODO: implement unscrambling/searching logic
-	// for a string of uppercase letters in the
-	// `input` parameter; return the array of
-	// matching words
-	return [];
+  // TODO: implement unscrambling/searching logic
+  // for a string of uppercase letters in the
+  // `input` parameter; return the array of
+  // matching words
+  return [];
 }
